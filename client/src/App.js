4417e8dc3647b9f9616ef04ffcee01e670eba47f
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-06-25 19:12:23 +0530

    made changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
-import logo from './logo.svg';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} /> 
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
