73d942cdaba21d75c86d675c8bb52bb26b9ed03d
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-06-25 19:21:12 +0530

    changes made to jsx files

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,19 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePages from './Pages/HomePage';  // Correct path and filename
+import RegisterPage from './pages/RegisterPage';  // Correct path and filename
+import LoginPage from './Pages/LoginPage';  // Correct path and filename
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} /> 
-      </Routes>
-      </BrowserRouter>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePages />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPge />} /> 
+        </Routes>
+      </Router>
     </div>
   );
 }
