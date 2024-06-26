fe896c72c5a03e7755964932de9df2c52eb754be
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-06-26 11:18:58 +0530

    adding form to registerpage

--- client/src/Pages/RegisterPage.jsx
+++ client/src/Pages/RegisterPage.jsx
@@ -2,7 +2,49 @@ import React from 'react'
 
 const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <div className='register'>
+      <div className='register_content'>
+         <form action="">
+          <input type="text" 
+          placeholder='First Name'
+          name='firstname'
+          required
+          />
+          <input type="text" 
+          placeholder='Last Name'
+          name='lastname'
+          required
+          />
+          <input type="email" 
+          placeholder='Email'
+          name='email'
+          required
+          />
+          <input type="password" 
+          placeholder='Password'
+          name='password'
+          required
+          />
+          <input type="password" 
+          placeholder='Confirm Password'
+          name='confirmpassword'
+          required
+          />
+          <input type="file" 
+           name='profileImage' accept='image/*'
+           style={{display:"none"}}
+           required
+           />
+           <label>
+            <img src="" alt="" />
+
+           </label>
+           
+         </form>
+         <a href='/login'>Already Have an account?Log in Here</a>
+      </div>
+    
+    </div>
   )
 }
 
