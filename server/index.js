b9badee18a2d95db135224e2f802d96c7361e785
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-06-26 17:17:53 +0530

    set up server and mongo db cloud

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express= require("express")
+const app=express()
+const mongoose= require("mongoose")
+const dotenv= require("dotenv").config()
+const cors=require("cors")
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+app.use(express.static('public'))
