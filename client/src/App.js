c4e11c314b7ffa591ff916f1d637d45cbd30a2e0
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-06-26 12:04:42 +0530

    changes to src

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,21 @@
 // App.js
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
 import HomePage from './Pages/HomePage';
 import RegisterPage from './Pages/RegisterPage';
 import LoginPage from './Pages/LoginPage';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/login" element={<LoginPage />} />
-      </Routes>
-    </Router>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
-
 export default App;
--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const HomePage = () => {
   return (
+    <>
     <div>Hello </div>
+    </>
   )
 }
 
-export default HomePage;
+export default HomePage
