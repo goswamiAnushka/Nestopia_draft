78e21e314b41fd3dc345db6e3b53f627f688e4c3
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-06-27 10:20:06 +0530

    made changes

--- client/src/Pages/LoginPage.jsx
+++ client/src/Pages/LoginPage.jsx
@@ -1,71 +1,9 @@
-import React, { useState } from "react";
-import "../styles/Login.scss"
-import { setLogin } from "../redux/state";
-import { useDispatch } from "react-redux"
-import { useNavigate } from "react-router-dom"
+import React from 'react'
 
 const LoginPage = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const dispatch = useDispatch()
-
-  const navigate = useNavigate()
-
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-
-    try {
-      const response = await fetch ("http://localhost:3001/auth/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ email, password })
-      })
-
-      /* Get data after fetching */
-      const loggedIn = await response.json()
-
-      if (loggedIn) {
-        dispatch (
-          setLogin({
-            user: loggedIn.user,
-            token: loggedIn.token
-          })
-        )
-        navigate("/")
-      }
-
-    } catch (err) {
-      console.log("Login failed", err.message)
-    }
-  }
-
   return (
-    <div className="login">
-      <div className="login_content">
-        <form className="login_content_form" onSubmit={handleSubmit}>
-          <input
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-          <button type="submit">LOG IN</button>
-        </form>
-        <a href="/register">Don't have an account? Sign In Here</a>
-      </div>
-    </div>
-  );
-};
+    <div>LoginPage</div>
+  )
+}
 
-export default LoginPage;
+export default LoginPage
