472d2ee1ed98d753787d976e5c813aa989960a3b
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-06-27 11:15:22 +0530

    updated env file

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use("/users", userRoutes)
 const PORT = 3001;
 mongoose
   .connect(process.env.MONGO_URL, {
-    dbName: "Dream_Nest",
+    dbName: "Nestopia",
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
