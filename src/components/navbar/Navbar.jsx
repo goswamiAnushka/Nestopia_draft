9d0da71659bb953349d531aa32e78d268b551a58
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-06-29 00:52:42 +0530

    updating style for navbar?

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,6 +3,14 @@ function Navbar() {
     return (
         <nav>
           <div className="left">left</div>
+          <a href="/">
+          <img src="/logo.png" alt="" />
+          <span>Nestopia</span>
+          </a>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
           <div className="right">Right</div>
         
         </nav>
