9b2582c354b68e83959bb43b580482acb502a26e
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-06-29 00:20:33 +0530

    updating navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+function Navbar() {
+    return (
+        <nav>
+         <div className="left">left</div>
+         <div className="right"></div>
+        
+        </nav>
+}
+
+export default Navbar;
