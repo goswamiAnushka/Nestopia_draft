c380a9e3dfbb6ddc92ade5bd890c6de1f4260705
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-06-29 21:46:29 +0530

    added search bar

--- src/routes/homePage/homePage.jsx
+++ src/routes/homePage/homePage.jsx
@@ -3,7 +3,21 @@ import './homePage.scss'; // Ensure the file path is correct
 function HomePage() {
     return (
         <div className="homePage">
-            <div className="textContainer">text</div>
+            <div className="textContainer">
+                <div className="wrapper">
+                    <div className="title">
+                        <h1>Your Journey to a New Home Starts Here</h1>
+                    <p>
+                    We are a team of dedicated real estate professionals passionate about helping you find your dream home. 
+                    With years of experience and a deep understanding of the market, we offer personalized service to meet your unique needs.
+                    Whether you're looking to rent, buy, or invest, we provide expert guidance and support from start to finish. 
+                    Our goal is to make the process smooth and enjoyable, ensuring you find a home that perfectly fits your 
+                    lifestyle and budget.Let us help you turn your homeownership dreams into reality.
+                    </p>
+                    
+                    </div>
+                </div>
+            </div>
             <div className="imgContainer">
                 <img src="/bg.png" alt="" className="bg" />
             </div>
