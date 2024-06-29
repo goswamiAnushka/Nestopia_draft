460d3c747ea8067ba3956a4cde50aa4250d4fe47
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-06-29 12:22:49 +0530

    fixing navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import "./navbar.scss";
-import { Link } from "react-router-dom";
 
 function Navbar() {
   const [open, setOpen] = useState(false);
@@ -19,26 +18,11 @@ function Navbar() {
         <a href="/">Agents</a>
       </div>
       <div className="right">
-        {user ? (
-          <div className="user">
-            <img
-              src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-              alt=""
-            />
-            <span>John Doe</span>
-            <Link to="/profile" className="profile">
-              <div className="notification">3</div>
-              <span>Profile</span>
-            </Link>
-          </div>
-        ) : (
-          <>
             <a href="/">Sign in</a>
             <a href="/" className="register">
               Sign up
             </a>
-          </>
-        )}
+        
         <div className="menuIcon">
           <img
             src="/menu.png"
