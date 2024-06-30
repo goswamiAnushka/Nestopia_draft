ba66c504a4114f6a9334a695d4f9c43f84e556c9
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-06-30 13:52:39 +0530

    adding route files

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import {
 } from "react-router-dom";
 import ListPage from "./routes/listPage/listPage";
 import Layout from "./routes/layout/layout";
-//import SinglePage from "./routes/singlePage/singlePage";
+import SinglePage from "./routes/singlePage/singlePage";
 //import ProfilePage from "./routes/profilePage/profilePage";
-//import Login from "./routes/login/login";
+import Login from "./routes/login/login";
 //import Register from "./routes/register/register";
 
 function App() {
--- src/routes/singlePage/singlePage.jsx
+++ src/routes/singlePage/singlePage.jsx
@@ -1,7 +1,7 @@
 import "./singlePage.scss";
-//import Slider from "../../components/slider/Slider";
-//import Map from "../../components/map/Map";
-//import { singlePostData, userData } from "../../lib/dummydata";
+import Slider from "../../components/slider/Slider";
+import Map from "../../components/map/Map";
+import { singlePostData, userData } from "../../lib/dummydata";
 
 function SinglePage() {
   return (
