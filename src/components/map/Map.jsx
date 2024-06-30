a6b4193bcc6b225aaed44074140656e842127a90
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-06-30 20:26:39 +0530

    updated map

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -5,7 +5,7 @@ import Pin from '../pin/Pin';
 
 function Map({items}){
   return (
-    <MapContainer center={[52.4797, -1.90269]} zoom={7} scrollWheelZoom={false} className='map'>
+    <MapContainer center={[20.5937, 78.9629]} zoom={5} scrollWheelZoom={false} className='map'>
     <TileLayer
       attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
