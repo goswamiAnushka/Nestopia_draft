aff526b6c4962d2a7f659582186ec117b7cb0223
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-06-30 13:26:35 +0530

    added componenents

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./navbar.scss";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   const [open, setOpen] = useState(false);
@@ -10,7 +11,7 @@ function Navbar() {
       <div className="left">
         <a href="/" className="logo">
           <img src="/logo.png" alt="" />
-          <span>Nestopia</span>
+          <span>LamaEstate</span>
         </a>
         <a href="/">Home</a>
         <a href="/">About</a>
@@ -18,11 +19,26 @@ function Navbar() {
         <a href="/">Agents</a>
       </div>
       <div className="right">
+        {user ? (
+          <div className="user">
+            <img
+              src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+              alt=""
+            />
+            <span>John Doe</span>
+            <Link to="/profile" className="profile">
+              <div className="notification">3</div>
+              <span>Profile</span>
+            </Link>
+          </div>
+        ) : (
+          <>
             <a href="/">Sign in</a>
             <a href="/" className="register">
               Sign up
             </a>
-        
+          </>
+        )}
         <div className="menuIcon">
           <img
             src="/menu.png"
