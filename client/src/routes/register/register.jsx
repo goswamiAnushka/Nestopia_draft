81b09b7d4f5fa101bfc2810ad96c22e2eb674f6a
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-07-06 02:21:33 +0530

    commit

--- client/src/routes/register/register.jsx
+++ client/src/routes/register/register.jsx
@@ -2,15 +2,29 @@ import "./register.scss";
 import { Link } from "react-router-dom";
 
 function Register() {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setError("")
+    setIsLoading(true);
+    const formData = new FormData(e.target);
+
+    const username = formData.get("username");
+    const email = formData.get("email");
+    const password = formData.get("password");
+    
+    
+    
+  };
   return (
-    <div className="register">
+    <div className="registerPage">
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleSubmit}>
           <h1>Create an Account</h1>
           <input name="username" type="text" placeholder="Username" />
           <input name="email" type="text" placeholder="Email" />
           <input name="password" type="password" placeholder="Password" />
-          <button >Register</button>
+          <button disabled={isLoading}>Register</button>
+          {error && <span>{error}</span>}
           <Link to="/login">Do you have an account?</Link>
         </form>
       </div>
