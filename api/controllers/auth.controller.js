18bc95bef2ba73eb24040f7143cddebd43ad67bf
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-07-06 00:33:41 +0530

    completed auth

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -31,8 +31,53 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = (req, res) => {
-  // Handle login logic, such as checking the database and validating credentials
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    // CHECK IF THE USER EXISTS
+
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
+
+    if (!user) return res.status(400).json({ message: "Invalid Credentials!" });
+
+    // CHECK IF THE PASSWORD IS CORRECT
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid)
+      return res.status(400).json({ message: "Invalid Credentials!" });
+
+    // GENERATE COOKIE TOKEN AND SEND TO THE USER
+
+    // res.setHeader("Set-Cookie", "test=" + "myValue").json("success")
+    const age = 1000 * 60 * 60 * 24 * 7;
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        isAdmin: false,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: age }
+    );
+
+    const { password: userPassword, ...userInfo } = user;
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        // secure:true,
+        maxAge: age,
+      })
+      .status(200)
+      .json(userInfo);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to login!" });
+  }
 };
 
 export const logout = (req, res) => {
