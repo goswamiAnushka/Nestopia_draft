69c31e64b55f0b0987e08d95f8a5f69b3841e1f1
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-07-06 00:52:13 +0530

    commit

--- api/app.js
+++ api/app.js
@@ -1,12 +1,13 @@
 // app.js
 import express from "express";
+import cookieParser from "cookie-parser";
 import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
 const app = express();
 app.use(express.json())
 app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
-
+app.use(cookieParser)
 app.listen(8800, () => {
     console.log("Server is running on http://localhost:8800");
 });
