a298ad10ccf04a64fa897f04e0dfe5274db72fd6
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-07-06 12:49:40 +0530

    succesfully connected register be and fe

--- api/app.js
+++ api/app.js
@@ -1,15 +1,26 @@
-// app.js
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import postRoute from "./routes/post.route.js";
 import authRoute from "./routes/auth.route.js";
+//import postRoute from "./routes/post.route.js";
+//import testRoute from "./routes/test.route.js";
+//import userRoute from "./routes/user.route.js";
+//import chatRoute from "./routes/chat.route.js";
+//import messageRoute from "./routes/message.route.js";
+
 const app = express();
-app.use(cors({origin:process.env.CLIENT_URL,credentials:true}));
-app.use(express.json())
-app.use("/api/posts", postRoute);
+
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth", authRoute);
-app.use(cookieParser)
+//app.use("/api/users", userRoute);
+//app.use("/api/posts", postRoute);
+//app.use("/api/test", testRoute);
+//app.use("/api/chats", chatRoute);
+//app.use("/api/messages", messageRoute);
+
 app.listen(8800, () => {
-    console.log("Server is running on http://localhost:8800");
+  console.log("Server is running!");
 });
--- client/src/routes/login/login.jsx
+++ client/src/routes/login/login.jsx
@@ -2,7 +2,7 @@ import { useContext, useState } from "react";
 import "./login.scss";
 import { Link, useNavigate } from "react-router-dom";
 import apiRequest from "../../lib/apiRequest";
-import { AuthContext } from "../../context/AuthContext";
+//import { AuthContext } from "../../context/AuthContext";
 
 function Login() {
   const [error, setError] = useState("");
