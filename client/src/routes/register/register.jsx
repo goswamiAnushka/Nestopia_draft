2a71b29739e16996bed73ab6f69561330bb088ef
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-07-06 11:19:47 +0530

    connecting backend to client

--- client/src/routes/register/register.jsx
+++ client/src/routes/register/register.jsx
@@ -1,7 +1,15 @@
 import "./register.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useState } from "react";
+
 
 function Register() {
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     setError("")
@@ -11,9 +19,20 @@ function Register() {
     const username = formData.get("username");
     const email = formData.get("email");
     const password = formData.get("password");
-    
-    
-    
+
+    try {
+      const res = await apiRequest.post("/auth/register", {
+        username,
+        email,
+        password,
+      });
+
+      navigate("/login");
+    } catch (err) {
+      setError(err.response.data.message);
+    } finally {
+      setIsLoading(false);
+    }
   };
   return (
     <div className="registerPage">
