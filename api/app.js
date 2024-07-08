b50cbd3f07a6f3359e83626c505099531ae1fc98
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-07-08 11:27:15 +0530

    commit

--- api/app.js
+++ api/app.js
@@ -5,8 +5,8 @@ import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
 import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
-//import chatRoute from "./routes/chat.route.js";
-//import messageRoute from "./routes/message.route.js";
+import chatRoute from "./routes/chat.route.js";
+import messageRoute from "./routes/message.route.js";
 
 const app = express();
 
@@ -18,8 +18,8 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/test", testRoute);
-//app.use("/api/chats", chatRoute);
-//app.use("/api/messages", messageRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
 
 app.listen(8800, () => {
   console.log("Server is running!");
