f2a767b8e637f34b35ef52e1711a0fc1ed62ce9f
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-07-09 19:39:31 +0530

    commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  baseURL: 'https://nestopia-backend.onrender.com', // Adjust according to your deployment environment
+});
