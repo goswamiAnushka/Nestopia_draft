a1fbbdcceab27888aa890b509e43c0ae926871b4
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-07-09 21:53:25 +0530

    change apireq

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://nestopia-backend.onrender.com",
+  baseURL: "http://localhost:8800/api",
   withCredentials: true,
 });
 
