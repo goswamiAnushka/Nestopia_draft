7114c15364f91aedca45933d185fcfe837f49bc3
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-07-09 19:21:26 +0530

    added backed url

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: "https://nestopia-backend.onrender.com/api",
   withCredentials: true,
 });
 
