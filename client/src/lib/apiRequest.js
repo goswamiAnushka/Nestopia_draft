997df6222aec5c4a86bca582a40b1094393f2d78
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-07-10 20:19:42 +0530

    changed baseurl

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800/api",
+  baseURL: '${window.location.origin}/api',
   withCredentials: true,
 });
 
