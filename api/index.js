7621c31d2d7dba57a15547fbd46f5bb52da63d1f
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-07-10 16:43:54 +0530

    commit server

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ import chatRoute from "./routes/chat.route.js";
 import messageRoute from "./routes/message.route.js";
 
 const app = express();
-const port = process.env.PORT || 5174;
+const port = process.env.PORT || 8800;
 
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
@@ -22,6 +22,11 @@ app.use("/api/test", testRoute);
 app.use("/api/chats", chatRoute);
 app.use("/api/messages", messageRoute);
 
-app.listen(8800, () => {
-  console.log("Server is running!");
+// Health check route
+app.get("/api/health", (req, res) => {
+  res.status(200).send("Server is healthy!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}!`);
 });
