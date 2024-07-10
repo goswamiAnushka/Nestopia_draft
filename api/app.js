3b916eacaabf1fd4da98512fcf75b64a89b4f29c
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-07-10 16:34:04 +0530

    commit

--- api/app.js
+++ api/app.js
@@ -9,6 +9,7 @@ import chatRoute from "./routes/chat.route.js";
 import messageRoute from "./routes/message.route.js";
 
 const app = express();
+const port = process.env.PORT || 5174;
 
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
