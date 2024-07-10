90033ace5aa97af71e4e8783c4aa167dc02ba9b1
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-07-11 03:29:03 +0530

    server commit

--- api/socket.js
+++ api/socket.js
@@ -1,34 +1,35 @@
-const { Server } = require("socket.io");
+const { Server } = require('socket.io');
 
 let io;
 
 const init = (server) => {
   io = new Server(server, {
     cors: {
-      origin: "https://nestopia.netlify.app/", // Adjust as needed for your security policies
+      origin: process.env.CLIENT_URL, // Ensure this matches the frontend URL
+      credentials: true,
     },
   });
 
-  io.on("connection", (socket) => {
-    console.log("A user connected");
+  io.on('connection', (socket) => {
+    console.log('A user connected');
 
-    socket.on("sendMessage", (data) => {
-      io.to(data.receiverId).emit("getMessage", data);
+    socket.on('sendMessage', (data) => {
+      io.to(data.receiverId).emit('getMessage', data);
     });
 
-    socket.on("joinChat", (userId) => {
+    socket.on('joinChat', (userId) => {
       socket.join(userId);
     });
 
-    socket.on("disconnect", () => {
-      console.log("A user disconnected");
+    socket.on('disconnect', () => {
+      console.log('A user disconnected');
     });
   });
 };
 
 const getIO = () => {
   if (!io) {
-    throw new Error("Socket.io not initialized!");
+    throw new Error('Socket.io not initialized!');
   }
   return io;
 };
