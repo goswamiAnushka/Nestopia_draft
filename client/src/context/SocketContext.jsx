3b0431ad47355f209e76cbc6daa43cd19d31bc43
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-07-10 20:27:29 +0530

    commit

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -9,7 +9,7 @@ export const SocketContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    setSocket(io("http://localhost:4000"));
+    setSocket(io("http://localhost:8800"));
   }, []);
 
   useEffect(() => {
