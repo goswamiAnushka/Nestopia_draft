43c65f5b41a788aa5ed82abbd0b1dffb72d32a9f
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-07-20 01:00:29 +0530

    commit

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://nestopia-draft-api.vercel.app/", // Adjust as per your server setup
+  baseURL: "https://nestopia-draft-api2.vercel.app/", // Adjust as per your server setup
   withCredentials: true,
 });
 
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  baseURL: 'https://nestopia-draft-api.vercel.app/'
+  baseURL: 'https://nestopia-draft-api2.vercel.app/'
 });
