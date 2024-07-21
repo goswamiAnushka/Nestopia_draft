ff8aa8d48270f9eecb04b5cc6723a129ef161e3f
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-07-21 23:55:56 +0530

    configured vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
+// Define the configuration
 export default defineConfig({
   plugins: [react()],
-  baseURL: 'http://localhost:8800', // Adjust according to your deployment environment
+  base: '/', // Adjust according to your deployment environment
+  build: {
+    outDir: 'dist', // Output directory for the build files
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'src/main.jsx'), // Path to the main entry file
+        index: path.resolve(__dirname, 'index.html'), // Path to the HTML file
+      },
+    },
+  },
 });
