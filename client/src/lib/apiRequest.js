51913c805ab8cefbed71bb05ec64880f3bc41c06
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-07-22 01:07:45 +0530

    commit npm

--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8800/api", // Adjust as per your server setup
+  baseURL: "https://nestopia-draft-backend.vercel.app/api", // Adjust as per your server setup
   withCredentials: true,
 });
 
